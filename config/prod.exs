3eeb7e6da3f64752ce36f27d848f4d1b71f808cb
Jean Rodriguez <jeanrodriguez24@gmail.com>
2016-05-19 20:26:23 -0400

    Second try

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "fathomless-lowlands-56409.herokuapp.com", port: ],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
